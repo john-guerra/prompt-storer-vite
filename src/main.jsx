05ed4642ab5010bc5c3f64ad015ce0816860f238
John Alexis Guerra Gómez <john.guerra@gmail.com>
2024-02-08 18:33:15 -0800

    Adding react router and basic pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import IndexPage from "./pages/IndexPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/",
+    element: <IndexPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
