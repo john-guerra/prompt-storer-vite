158e8d7178b72044fef1410a16da27fc1bba49fe
John Alexis Guerra Gómez <john.guerra@gmail.com>
2024-03-14 18:07:21 -0700

    Translating the About page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import BaseTemplate from "../templates/BaseTemplate";
 
 import "./App.css";
 
-class AboutPage extends Component {
-  render() {
-    const urlParams = new URLSearchParams(new URL(window.location.href).search);
-    const id = urlParams.get("id");
+function AboutPage(props) {
+  const urlParams = new URLSearchParams(new URL(window.location.href).search);
+  const id = urlParams.get("id");
 
-    console.log("Render AboutPage", this.props);
-    return (
-      <div className="App">
-        <BaseTemplate>
-          <h1>About {id}</h1>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse alias
-            suscipit aliquid? Rerum eos quibusdam earum consequatur odit. Velit
-            illum fuga quidem quaerat facere quasi, dignissimos fugit magni
-            repellendus nisi.
-          </div>
-        </BaseTemplate>
-      </div>
-    );
-  }
+  console.log("Render AboutPage", props);
+  return (
+    <div className="App">
+      <BaseTemplate>
+        <h1>About {id}</h1>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse alias
+          suscipit aliquid? Rerum eos quibusdam earum consequatur odit. Velit
+          illum fuga quidem quaerat facere quasi, dignissimos fugit magni
+          repellendus nisi.
+        </div>
+      </BaseTemplate>
+    </div>
+  );
 }
 
 export default AboutPage;
